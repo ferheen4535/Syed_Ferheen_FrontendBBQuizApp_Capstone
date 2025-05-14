af296b9d2fe5781c36274463ba34b338a3b78658
ferheen4535 <ferheen.syed@gmail.com>
2025-05-14 11:57:50 -0400

    revise6

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 /* API base */
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5055';
+const API = import.meta.env.VITE_API_URL || 'https://syed-ferheen-backendproductquizapp.onrender.com/';
 
 /* helper */
 const createUser = async (userData) =>
